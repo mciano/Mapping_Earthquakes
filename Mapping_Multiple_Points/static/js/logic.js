6d15107830ba4dfb5a42b79d17c169072d4a8cc2
Marcio <92833805+mciano@users.noreply.github.com>
2022-01-26 23:11:12 -0500

    multiple points

---
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -0,0 +1,35 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create a map object
+var map = L.map("mapid", {
+  center: [37.09, -95.71],
+  zoom: 4
+
+});
+
+// Add a tile layer
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data © <a href= "https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/dark-v10",
+  accessToken: API_KEY
+}).addTo(map);
+
+
+
+// Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+  console.log(city)
+  L.circleMarker(city.location, {
+    radius: city.population/200000,
+    color: "orange"
+  })
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
+});
